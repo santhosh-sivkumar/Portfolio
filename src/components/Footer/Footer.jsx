e0ecff323f4788b70eba5d77f60a08e1a738e1fd
santhosh-sivkumar <santhosh.sivkumar@gmail.com>
2024-05-15 14:46:20 +0530

    Update navigation items and social links color in Footer component
    - Update `navigationItems` array from ["About", "Projects"] to ["About", "Portfolio"]
    - Change color of `footer__link` to `--title

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./footer.css";
 
-const navigationItems = ["About", "Projects", "Services"];
+const navigationItems = ["About", "Portfolio", "Services"];
 
 const socialLinks = [
   {
