e9384a4578d328f4eff332a06b3ee1f41411b3cd
santhosh-sivkumar <santhosh.sivkumar@gmail.com>
2024-05-16 01:24:10 +0530

    Add React Router and update index.js to use Router and Routes
    The diff includes the following changes:
    - Addition of React Router packages to package.json and package-lock.json
    - Addition of meta

--- src/index.js
+++ src/index.js
@@ -1,17 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import "./index.css";
+import About from "./components/About/About";
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+import Skills from "./components/Skills/Skills";
+import Services from "./components/Services/Services";
+import Qualification from "./components/Qualification/Qualification";
+import Work from "./components/Work/Work";
+import Contact from "./components/Contact/Contact";
+import Footer from "./components/Footer/Footer";
+import ScrollUp from "./components/ScrollUp/ScrollUp";
+import Themes from "./components/Themes/Themes";
+class Portfolio extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Header />
+        <main className="main">
+          <Home />
+          <Themes />
+          <About />
+          <Skills />
+          <Services />
+          <Qualification />
+          <Work />
+          <Contact />
+        </main>
+        <Footer />
+        <ScrollUp />
+      </React.Fragment>
+    );
+  }
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Router>
+    <Routes>
+      <Route path="/portfolio-of-santhosh-sivakumar" element={<Portfolio />} />
+    </Routes>
+  </Router>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
