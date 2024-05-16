c20713d8b41cc66a43e661d2378dd7a83582e41e
santhosh-sivkumar <santhosh.sivkumar@gmail.com>
2024-05-17 04:10:56 +0530

    removed hand svg from home

--- src/components/About/Info.jsx
+++ src/components/About/Info.jsx
@@ -14,7 +14,7 @@ const Info = () => {
         <i className="bx bx-briefcase-alt about__icon"></i>
 
         <h3 className="about__title">Completed</h3>
-        <span className="about__subtitle">7+ Projects</span>
+        <span className="about__subtitle">10+ Projects</span>
       </div>
 
       <div className="about__box">
