980d73db26e39d2ff53322aa2567ff5a01754879
santhosh-sivkumar <santhosh.sivkumar@gmail.com>
2024-06-09 02:30:48 +0530

    "Update title of project with id 8 to 'Youtube + Studio Clone'"

--- src/components/Work/Data.jsx
+++ src/components/Work/Data.jsx
@@ -68,7 +68,7 @@ export const projectsData = [
   {
     id: 8,
     image: YoutubeClone_Preview,
-    title: "Youtube Clone",
+    title: "Youtube + Studio Clone",
     category: "web",
     demolink: "https://sansiv-youtube-clone.netlify.app/",
     codelink: "https://github.com/santhosh-sivkumar/youtube-clone",
